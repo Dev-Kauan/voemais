2939058fc585508b3c9eae7b16843b5728693276
Dev-Kauan <kauanlima127@gmail.com>
2024-09-29 16:04:11 -0300

    feat: CREATE e READ do voo.

---
+++ src/app/voo/create/page.js
@@ -0,0 +1,116 @@
+'use client'
+
+import Pagina from "@/app/components/Pagina";
+import { Formik } from "formik";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Button, Form } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
+import { FaAngleLeft } from "react-icons/fa";
+
+export default function Page() {
+   
+    const route = useRouter()
+
+    function salvar(dados) {
+        const voo = JSON.parse(localStorage.getItem('voo')) || []
+        voo.push(dados);
+        localStorage.setItem('voo', JSON.stringify(voo))
+        return route.push('/voo')
+    }
+    
+    return (
+        <Pagina titulo="Voo">
+            <Formik
+                initialValues={{ internacional: '', identificador: '', data_checkin: '', data_embarque: '', origem: '', destino: '', empresa: '', preco: '' }}
+                onSubmit={values => salvar(values)}
+            >
+                {({
+                    values,
+                    handleChange,
+                    handleSubmit,
+                }) => (
+                    <Form className="mt-3">
+                        <Form.Group className="mb-3" controlId="internacional">
+                            <Form.Label>Internacional</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite se é internacional ou não"
+                                name="internacional"
+                                value={values.internacional}
+                                onChange={handleChange('internacional')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="identificador">
+                            <Form.Label>Identificador</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Digite identificador"
+                                name="identificador"
+                                value={values.identificador}
+                                onChange={handleChange('identificador')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="data_checkin">
+                            <Form.Label>Data Checkin</Form.Label>
+                            <Form.Control type="date"
+                                name="data_checkin"
+                                value={values.data_checkin}
+                                onChange={handleChange('data_checkin')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="data_embarque">
+                            <Form.Label>Data Embarque</Form.Label>
+                            <Form.Control type="date"
+                                name="data_embarque"
+                                value={values.data_embarque}
+                                onChange={handleChange('data_embarque')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="origem">
+                            <Form.Label>Origem</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Digite a origem"
+                                name="origem"
+                                value={values.origem}
+                                onChange={handleChange('origem')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="destino">
+                            <Form.Label>Destino</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Digite o destino"
+                                name="destino"
+                                value={values.destino}
+                                onChange={handleChange('destino')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="empresa">
+                            <Form.Label>Empresa</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Digite o nome da empresa"
+                                name="empresa"
+                                value={values.empresa}
+                                onChange={handleChange('empresa')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="preco">
+                            <Form.Label>Preço</Form.Label>
+                            <Form.Control type="number"
+                                placeholder="Digite o preço do voo"
+                                name="preco"
+                                value={values.preco}
+                                onChange={handleChange('preco')}
+                            />
+                        </Form.Group>
+                        <div className="text-center">
+                            <Link href={"/voo"} className="btn btn-primary"><FaAngleLeft />Voltar</Link>
+                            <Button variant="success" className="ms-1" onClick={handleSubmit}>
+                                <FaCheck />Salvar
+                            </Button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </Pagina>
+    )
+}
---
+++ src/app/voo/page.js
@@ -0,0 +1,51 @@
+'use client'
+
+import Pagina from "@/app/components/Pagina";
+import Link from "next/link";
+import { Table } from "react-bootstrap";
+import { FaPlusCircle } from "react-icons/fa";
+
+export default function Page() {
+
+    const voo = JSON.parse(localStorage.getItem('voo')) || []
+
+    return (
+        <Pagina titulo="Voos">
+
+            <Link href={"/voo/create"} className="btn btn-primary mb-3 mt-3">
+                <FaPlusCircle /> Novo
+            </Link>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Internacional</th>
+                        <th>Identificador</th>
+                        <th>Data Checkin</th>
+                        <th>Data Embarque</th>
+                        <th>Origem</th>
+                        <th>Destino</th>
+                        <th>Empresa</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {voo.map((item, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td> {/* Exibindo o índice como ID */}
+                            <td>{item.internacional}</td>
+                            <td>{item.identificador}</td>
+                            <td>{item.data_checkin}</td>
+                            <td>{item.data_embarque}</td>
+                            <td>{item.origem}</td>
+                            <td>{item.destino}</td>
+                            <td>{item.empresa}</td>
+                            <td>{item.preco}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </Pagina>
+    )
+}
